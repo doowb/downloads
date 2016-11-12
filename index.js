d8bd95398d2971876c805c30e7cf6f3219f7e9c8
doowb <brian.woodward@gmail.com>
2016-11-12 16:41:12 -0500

    update to expose separate methods for getting repo or maintainer downloads add event emitter to emit results when they happen (useful when getting maintainer repos and wanting to see the status)

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
 
 var co = require('co');
 var npm = require('npm-api')();
+var limiter = require('co-limiter');
 var extend = require('extend-shallow');
+var Emitter = require('component-emitter');
 
-module.exports = function(options, cb) {
+var downloads = Emitter({});
+
+downloads.repo = function(name, options, cb) {
   return co(function*() {
-    if (typeof options === 'string') {
-      options = {repo: options};
+    if (typeof name !== 'string') {
+      throw new TypeError('expected `name` to be a string');
+    }
+
+    if (typeof options === 'function') {
+      cb = options;
+      options = {};
     }
 
     var opts = extend({}, options);
-    if (!opts.repo && !opts.maintainer) {
-      throw new Error('expected "options.repo" or "options.maintainer" to be set');
-    }
-
-    if (opts.repo) {
-      return yield npm.repo(opts.repo).downloads();
-    }
-
-    var maintainer = npm.maintainer(opts.maintainer);
-    var repos = yield maintainer.repos();
-    return repos.map(function(name) {
-      var repo = npm.repo(name);
-      return repo.downloads();
-    });
+    var results = yield npm.repo(name).downloads();
+    downloads.emit('repo', name, results);
+    return results;
   })
   .then(function(result) {
     if (typeof cb === 'function') {
@@ -40,3 +38,51 @@ module.exports = function(options, cb) {
     throw err;
   });
 };
+
+downloads.maintainer = function(name, options, cb) {
+  return co(function*() {
+    if (typeof name !== 'string') {
+      throw new TypeError('expected `name` to be a string');
+    }
+
+    if (typeof options === 'function') {
+      cb = options;
+      options = {};
+    }
+
+    var opts = extend({limit: 1}, options);
+    var limit = limiter(opts.limit);
+
+    var maintainer = npm.maintainer(name);
+    var repos = yield maintainer.repos();
+    downloads.emit('repos', name, repos);
+
+    var results = {};
+    yield repos.map(function(repo) {
+      return co(function*() {
+        results[repo] = yield limit(run(repo));
+      });
+    });
+    downloads.emit('maintainer', name, results);
+    return results;
+  })
+  .then(function(result) {
+    if (typeof cb === 'function') {
+      cb(null, result);
+      return;
+    }
+    return result;
+  }, function(err) {
+    if (typeof cb === 'function') {
+      cb(err);
+      return;
+    }
+    throw err;
+  });
+};
+
+function* run(name) {
+  return yield downloads.repo(name);
+}
+
+module.exports = downloads;
