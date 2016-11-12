ca8a76257064236ac02a97df47354cf7a4c1e84e
doowb <brian.woodward@gmail.com>
2016-11-12 15:48:17 -0500

    initial implementation

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 'use strict';
 
-module.exports = function() {
+var co = require('co');
+var npm = require('npm-api')();
+var extend = require('extend-shallow');
+
+module.exports = function(options, cb) {
+  return co(function*() {
+    var opts = extend({}, options);
+    if (!opts.repo && !opts.maintainer) {
+      throw new Error('expected "options.repo" or "options.maintainer" to be set');
+    }
+
+    if (opts.repo) {
+      return yield npm.repo(opts.repo).downloads();
+    }
+
+    var maintainer = npm.maintainer(opts.maintainer);
+    var repos = yield maintainer.repos();
+    return repos.map(function(name) {
+      var repo = npm.repo(name);
+      return repo.downloads();
+    });
+  })
+  .then(function(result) {
+    if (typeof cb === 'function') {
+      cb(null, result);
+      return;
+    }
+    return result;
+  }, function(err) {
+    if (typeof cb === 'function') {
+      cb(err);
+      return;
+    }
+    throw err;
+  });
 };
